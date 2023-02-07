f7db0e0027365ebc4aec069afaac1f85e3642a62
M. Angel Moreno <59739569+mangelm@users.noreply.github.com>
2023-02-07 05:39:41 +0100

    Add files via upload

--- battle/main.py
+++ battle/main.py
@@ -13,4 +13,3 @@ magic = [{"name":"Blizzard",
 player = Person(460, 65, 60, 34,magic)
 print(player.generate_spell_damage(0))
 print(player.generate_spell_damage(1))
-#print(player.generate_damage())
